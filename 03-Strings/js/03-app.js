5ee576da4368f713a432b7da70caf01e33507b64
Rafael <rafael.ifcd0110@gmail.com>
2024-09-10 18:56:35 +0100

    actualizado

---
+++ 03-Strings/js/03-app.js
@@ -0,0 +1,5 @@
+// como concatenar y unir cadenas de texto y variables
+const producto = "Monitor de 30 pulgadas";
+const precio = "200";
+
+console.log(producto.concat(" En descuento")); // concatenar un string
