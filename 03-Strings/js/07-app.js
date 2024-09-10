80cf21118b778139117da248835835c7a65eeaa5
Rafael <rafael.ifcd0110@gmail.com>
2024-09-10 20:00:57 +0100

    actualizacion

---
+++ 03-Strings/js/07-app.js
@@ -0,0 +1,11 @@
+//toUpperCase te va a permitir cambiar un texto a mayúsculas
+const producto = "Televisor 55 pulgadas";
+console.log(producto.toUpperCase());
+//toLowerCase cambia el texto a minúsculas
+console.log(producto.toLowerCase());
+
+//toString Convierte la varible en un texto
+const cantidad = 200;
+console.log(cantidad);
+console.log(cantidad.toString());
+
