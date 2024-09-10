cb0f6e7850695a699e93d1a9cb79cf6e83a67178
Rafael <rafael.ifcd0110@gmail.com>
2024-09-10 17:04:22 +0100

    actualizacion

---
+++ 02- Variables/js/02-app.js
@@ -0,0 +1,15 @@
+// --var-- era la forma de declarar variables en versiones anteriores
+// hoy se usa --let--
+
+let producto = "pantalla";
+
+console.log(producto);
+
+//a pesar de que la variable es de tipo string
+//podemos asignarle otro valor
+
+producto = 20;
+producto = true;
+producto = null;
+
+console.log(producto);
---
+++ 02- Variables/js/03-app.js
@@ -0,0 +1,14 @@
+// vamos al 3er tipo de declaración de variables -- const --
+// diferencias entre --let-- y --const--
+// let: permite reasignación del valor  - const: no
+// let y const tienen alcance de bloque {}
+// let: permite inicializar sin valor  - const: no
+
+const producto = "Monitor";
+console.log(producto);
+
+const precio = 200;
+
+// precio = 300; ***no se puede
+
+console.log(precio);
