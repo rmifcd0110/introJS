d2003cb89f3af1d9d89c47a6f5b9c347ee4558c4
Rafael <rafael.ifcd0110@gmail.com>
2024-09-12 18:16:37 +0100

    actualizado

---
+++ 09-Funciones/js/01-app.js
@@ -0,0 +1,14 @@
+/*Las funciones en cualquier lenguaje son una serie de procedimientos o instrucciones
+que realizan una acción. Una de las ventajas de las funciones es que permiten tener un código más ordenado y facil de mantener*/
+
+/**Otra de las ventajs de las funciones es que son reutilizables. Puedes tener una función que valide un formulario y utilizarla en todos los formularios. También puedes tener una función que envíe datos al servidor y reutilizarla miles de veces. */
+
+// Existen muchas formas de crear funciones en JavaScript
+
+//Declaración de función: la declara de la función utiliza la palabra reservada "function" 
+//seguida de un nombre y unos paréntesis. En estos paréntesis se colocan los argumentos.
+//Finalmente el cuerpo de la función de se define con las llaves.
+
+function sumar(){
+    console.log(2 + 2);
+}
