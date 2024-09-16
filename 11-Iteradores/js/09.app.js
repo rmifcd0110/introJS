637c082d30e9143bb4221bb2bc93c00fbddc82dd
Rafael <rafael.ifcd0110@gmail.com>
2024-09-16 17:06:28 +0100

    actualizado

--- 11-Iteradores/js/09.app.js
+++ 11-Iteradores/js/09.app.js
@@ -1,2 +1,29 @@
 //Realizar un script que pida el número de filas y columnas, cree una tabla y dentro de cada una de las celdas escriba un número consecutivo en orden descendente.
 
+function generarTabla(){
+    //Obtener los valores de filas y columnas
+    let filas = parseInt(document.getElementById('filas').value);
+    let columnas = parseInt(document.getElementById('columnas').value);
+
+    // Calculamos el total de celdas
+    let totalCeldas = filas * columnas;
+
+    //Creamos la tabla de forma dinámica
+    let tabla = "<table border='1' cellpadding='10 cellspacing='0'>";
+
+    //Empezamos a llenar la tabla
+    for(let i= 0; i < filas; i++){
+        tabla += "<tr>"; //nueva fila
+        for(let j = 0; j < columnas; j++){
+            tabla += "<td>" + totalCeldas + "</td>"
+            totalCeldas--;
+        }
+        tabla += "</tr>"; //Cerramos la fila
+    }
+
+    tabla += "</table>"; //Cerramos la tabla
+
+    //Insertar tabla en el div con id "tabla"
+
+    document.getElementById('tabla').innerHTML = tabla;
+}
---
+++ 11-Iteradores/js/10-app.js
@@ -0,0 +1,9 @@
+/**Tabla de multiplicación
+ * 1. El usuario debe ingresa las filas y las columnas de la tabla
+ * 2. Cada celda debe contener el producto de los números que representan la fila y la columna en la que se encuentran. Ej si la celda está en la fila 2 y columna 3, el valot en la celda debe ser 2 x 3 = 6.
+ * 3. El script debe generar la tabla de forma dinámica y mostrar el HTML
+ * 
+ */
+
+
+
